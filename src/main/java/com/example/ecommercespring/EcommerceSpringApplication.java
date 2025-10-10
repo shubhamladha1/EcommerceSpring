0fb8b23faf0fcb2693ac1f0ae8a8b6a8f889c5f2
shubhamladha <shubhamladha12@GMAIL.COM>
2025-10-10 16:35:30 +0530

    Added dotenv changes

--- src/main/java/com/example/ecommercespring/EcommerceSpringApplication.java
+++ src/main/java/com/example/ecommercespring/EcommerceSpringApplication.java
@@ -1,5 +1,6 @@
 package com.example.ecommercespring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceSpringApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue())); // Set system properties from environment variables and set them
+
 		SpringApplication.run(EcommerceSpringApplication.class, args);
 	}
 
